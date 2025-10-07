4f266f70a1ea53b6ff0fcb1792bd87ca5e2adffa
Jihoon <mica0915@snu.ac.kr>
2025-10-07 15:51:11 +0900

    5582 LCS

---
+++ 250830/250830.cpp
@@ -0,0 +1,23 @@
+//5582 LCS
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string s1, s2;
+int dp[4002][4002], res;
+
+int main() {
+    cin >> s1 >> s2;
+    for(int i = 1; i <= s1.length(); i++) {
+        for(int j = 1; j <= s2.length(); j++) {
+            if(s1[i-1] == s2[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            res = max(dp[i][j], res);
+        }
+    }
+    cout << res;
+    return 0;
+}
